9fd1ebf9b5e05744ee171633c0ee1bd51236a9cd
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2016-04-26 11:27:22 +0200

    Code reformatting

--- timeout.pl
+++ timeout.pl
@@ -1,10 +1,8 @@
 #!/usr/bin/perl
 
-## timeout
-##
-## (c) 2004-2007, 2016 Piotr Roszatycki <dexter@debian.org>, GPL
-##
-## $Id$
+# timeout
+#
+# (c) 2004-2007, 2016 Piotr Roszatycki <dexter@debian.org>, GPL
 
 =head1 NAME
 
@@ -37,76 +35,50 @@ use Config;
 use POSIX ();
 
 
-##############################################################################
+our $NAME = 'timeout';
 
-## Default values for constant variables
-##
-
-## Program name
-my $NAME = 'timeout';
-
-## Program version
-my $VERSION = '0.12';
+our $VERSION = '0.12';
 
 
-##############################################################################
+# Signals to handle
+my @signals = qw{HUP INT QUIT TERM SEGV PIPE XCPU XFSZ ALRM};
 
-## Signals to handle
-##
-my @signals = qw< HUP INT QUIT TERM SEGV PIPE XCPU XFSZ ALRM >;
-
-
-##############################################################################
-
-## Signal to send after timeout. Default is KILL.
+# Signal to send after timeout. Default is KILL.
 my $signal = 'KILL';
 
-## Time to wait
+# Time to wait
 my $time = 0;
 
-## Command to execute as array of arguments
+# Command to execute as array of arguments
 my @command = ();
 
-## PID for fork function
+# PID for fork function
 my $child_pid;
 
-## PID for wait function
+# PID for wait function
 my $pid;
 
 
-##############################################################################
-
-## usage()
-##
-## Prints usage message.
-##
-sub usage() {
+# Prints usage message.
+sub usage () {
     # Lazy loading for Pod::Usage
     eval 'use Pod::Usage;';
     die $@ if $@;
-
     pod2usage(2);
 }
 
 
-## help()
-##
-## Prints help message.
-##
-sub help() {
+# Prints help message.
+sub help () {
     # Lazy loading for Pod::Usage
     eval 'use Pod::Usage;';
     die $@ if $@;
-
     pod2usage(-verbose=>1, -message=>"$NAME $VERSION\n");
 }
 
 
-## signal_handler($sig)
-##
-## Handler for signals to clean up child processes
-##
-sub signal_handler($) {
+# Handler for signals to clean up child processes
+sub signal_handler ($) {
     my ($sig) = @_;
     if ($sig eq 'ALRM') {
         printf STDERR "Timeout: aborting command ``%s'' with signal SIG%s\n", join(' ', @command), $signal;
@@ -118,78 +90,80 @@ sub signal_handler($) {
 }
 
 
-##############################################################################
-
-## Main subroutine
-##
-
-
-## Parse command line arguments
-my $arg = $ARGV[0];
-usage() unless $arg;
-if ($arg =~ /^-(.*)$/) {
-    my $opt = $1;
-    if ($arg eq '-h' || $arg eq '--help') {
-        help();
-    } elsif ($opt =~ /^[A-Z0-9]+$/) {
-        if ($opt =~ /^\d+/) {
-	    # Convert numeric signal to name by using the perl interpreter's
-	    # configuration:
-            usage() unless defined $Config{sig_name};
-            $signal = (split(' ', $Config{sig_name}))[$opt];
+# Main subroutine
+sub main () {
+    # Parse command line arguments without Getopt::Long
+    my $arg = $ARGV[0];
+    usage() unless $arg;
+    if ($arg =~ /^-(.*)$/) {
+        my $opt = $1;
+        if ($arg eq '-h' || $arg eq '--help') {
+            help();
+        } elsif ($opt =~ /^[A-Z0-9]+$/) {
+            if ($opt =~ /^\d+/) {
+    	    # Convert numeric signal to name by using the perl interpreter's
+    	    # configuration:
+                usage() unless defined $Config{sig_name};
+                $signal = (split(' ', $Config{sig_name}))[$opt];
+            } else {
+                $opt =~ s/^SIG//;
+                $signal = $opt;
+            }
+    	shift @ARGV;
         } else {
-            $opt =~ s/^SIG//;
-            $signal = $opt;
+            usage();
         }
-	shift @ARGV;
-    } else {
-        usage();
     }
+
+    usage() if @ARGV < 2;
+
+    $arg = $ARGV[0];
+
+    usage() unless $arg =~ /^\d+$/;
+
+    $time = $arg;
+
+    shift @ARGV;
+
+    @command = @ARGV;
+
+
+    # Fork for exec
+    if (not defined($child_pid = fork)) {
+        die "Could not fork: $!\n";
+        exit 1;
+    } elsif ($child_pid == 0) {
+        # child
+
+        # Set new process group
+        POSIX::setsid;
+
+        # Execute command
+        exec @command or die "Can not run command `" . join(' ', @command) . "': $!\n";
+    }
+
+    # parent
+
+    # Set the handle for signals
+    foreach my $sig (@signals) {
+        $SIG{$sig} = \&signal_handler;
+    }
+
+    # Set the alarm
+    alarm $time;
+
+    # Wait for child
+    while (($pid = wait) != -1 && $pid != $child_pid) {}
+
+    # Clean exit
+    exit ($pid == $child_pid ? $? >> 8 : -1);
 }
 
-usage() if @ARGV < 2;
 
-$arg = $ARGV[0];
-
-usage() unless $arg =~ /^\d+$/;
-
-$time = $arg;
-
-shift @ARGV;
-
-@command = @ARGV;
+main();
 
 
-## Fork for exec
-if (! defined($child_pid = fork)) {
-    die "Could not fork: $!\n";
-    exit 1;
-} elsif ($child_pid == 0) {
-    ## child
-
-    ## Set new process group
-    POSIX::setsid;
-
-    ## Execute command
-    exec @command or die "Can not run command `" . join(' ', @command) . "': $!\n";
-}
-
-## parent
-
-## Set the handle for signals
-foreach my $sig (@signals) {
-    $SIG{$sig} = \&signal_handler;
-}
-
-## Set the alarm
-alarm $time;
-
-## Wait for child
-while (($pid = wait) != -1 && $pid != $child_pid) {}
-
-## Clean exit
-exit ($pid == $child_pid ? $? >> 8 : -1);
-
+__END__
 
 =head1 DESCRIPTION
 
@@ -220,15 +194,15 @@ The command to be executed.
 
 =over 8
 
-=item 0..253
+=item S<0..253>
 
 Return code from called command.
 
-=item 254
+=item S<254>
 
 Internal error. No return code could be fetched.
 
-=item 255
+=item S<255>
 
 The timeout was occured.
 
@@ -264,11 +238,11 @@ UNIX/System_administration
 
 =head1 AUTHORS
 
-Piotr Roszatycki E<lt>dexter@debian.orgE<gt>
+Piotr Roszatycki <dexter@debian.org>
 
 =head1 LICENSE
 
-Copyright 2004-2007, 2016 by Piotr Roszatycki E<lt>dexter@debian.orgE<gt>.
+Copyright 2004-2007, 2016 by Piotr Roszatycki <dexter@debian.org>.
 
 Inspired by timeout.c that is part of The Coroner's Toolkit.
 
